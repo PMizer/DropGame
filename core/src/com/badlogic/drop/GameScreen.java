93587cb56415ef83db156e4125d86fe0a1541db3
PMizer <mizer.piotr@gmail.com>
2022-06-27 22:11:09 +0200

    First version of game drop

---
+++ core/src/com/badlogic/drop/GameScreen.java
@@ -0,0 +1,201 @@
+package com.badlogic.drop;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.TimeUtils;
+
+import java.sql.Time;
+import java.util.Iterator;
+
+public class GameScreen implements Screen {
+	final Drop game;
+
+	Texture dropImage;
+	Texture bucketImage;
+	Texture birbImage;
+	Sound dropSound;
+	Sound birbSound;
+	Music rainMusic;
+	OrthographicCamera camera;
+	Rectangle bucket;
+	Array<Rectangle> raindrops;
+	Array<Rectangle> birbs;
+	long lastDropTime;
+	long lastBirbTime;
+	int dropsGathered;
+	int birbCounter;
+
+	public GameScreen(final Drop game) {
+		this.game = game;
+
+		// load the images for the droplet and the bucket, 64x64 pixels each
+		dropImage = new Texture(Gdx.files.internal("drop.png"));
+		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+		birbImage = new Texture(Gdx.files.internal("birb.png"));
+
+		// load the drop sound effect and the rain background "music"
+		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+		birbSound = Gdx.audio.newSound(Gdx.files.internal("birb_caw.mp3"));
+		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+		rainMusic.setLooping(true);
+
+		// create the camera and the SpriteBatch
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+
+		// create a Rectangle to logically represent the bucket
+		bucket = new Rectangle();
+		bucket.x = 800 / 2 - 64 / 2; // center the bucket horizontally
+		bucket.y = 20; // bottom left corner of the bucket is 20 pixels above
+		// the bottom screen edge
+		bucket.width = 64;
+		bucket.height = 64;
+
+		// create the raindrops array and spawn the first raindrop
+		raindrops = new Array<Rectangle>();
+		spawnRaindrop();
+
+		birbs = new Array<Rectangle>();
+		spawnBirb();
+
+	}
+	private void spawnBirb(){
+		Rectangle birb = new Rectangle();
+		birb.x = 0;
+		birb.y = MathUtils.random(240-64,480-64);
+		birb.width = 64;
+		birb.height =64;
+		birbs.add(birb);
+		lastBirbTime = TimeUtils.nanoTime();
+	}
+
+	private void spawnRaindrop() {
+		Rectangle raindrop = new Rectangle();
+		raindrop.x = MathUtils.random(0, 800 - 64);
+		raindrop.y = 480;
+		raindrop.width = 64;
+		raindrop.height = 64;
+		raindrops.add(raindrop);
+		lastDropTime = TimeUtils.nanoTime();
+	}
+
+	@Override
+	public void render(float delta) {
+
+		ScreenUtils.clear(0, 0, 0.2f, 1);
+
+		// tell the camera to update its matrices.
+		camera.update();
+
+		// tell the SpriteBatch to render in the
+		// coordinate system specified by the camera.
+		game.batch.setProjectionMatrix(camera.combined);
+
+		// begin a new batch and draw the bucket and
+		// all drops
+		game.batch.begin();
+		game.font.draw(game.batch, "Drops Collected: " + dropsGathered, 10, 470);
+		game.batch.draw(bucketImage, bucket.x, bucket.y, bucket.width, bucket.height);
+		for (Rectangle raindrop : raindrops) {
+			game.batch.draw(dropImage, raindrop.x, raindrop.y);
+		}
+		for(Rectangle birb : birbs){
+			game.batch.draw(birbImage,birb.x, birb.y);
+		}
+		game.batch.end();
+
+		// process user input
+		if (Gdx.input.isTouched()) {
+			Vector3 touchPos = new Vector3();
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			camera.unproject(touchPos);
+			bucket.x = touchPos.x - 64 / 2;
+		}
+		if (Gdx.input.isKeyPressed(Keys.LEFT))
+			bucket.x -= 200 * Gdx.graphics.getDeltaTime();
+		if (Gdx.input.isKeyPressed(Keys.RIGHT))
+			bucket.x += 200 * Gdx.graphics.getDeltaTime();
+
+		// make sure the bucket stays within the screen bounds
+		if (bucket.x < 0)
+			bucket.x = 0;
+		if (bucket.x > 800 - 64)
+			bucket.x = 800 - 64;
+
+		// check if we need to create a new raindrop
+		if (TimeUtils.nanoTime() - lastDropTime > 1000000000)
+			spawnRaindrop();
+		if(TimeUtils.nanoTime() - lastBirbTime > 1000000000)
+			spawnBirb();
+
+		// move the raindrops, remove any that are beneath the bottom edge of
+		// the screen or that hit the bucket. In the later case we increase the
+		// value our drops counter and add a sound effect.
+		Iterator<Rectangle> iter = raindrops.iterator();
+		Iterator<Rectangle> iterBirb = birbs.iterator();
+		while(iterBirb.hasNext()){
+			Rectangle birb = iterBirb.next();
+			birb.x += MathUtils.random(50,250) * Gdx.graphics.getDeltaTime();
+			if(birb.x+64>800) {
+				iterBirb.remove();
+				birbCounter++;
+				if (birbCounter % 3 == 0)
+					birbSound.play();
+			}
+
+		}
+		while (iter.hasNext()) {
+			Rectangle raindrop = iter.next();
+			raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+			if (raindrop.y + 64 < 0)
+				iter.remove();
+			if (raindrop.overlaps(bucket)) {
+				dropsGathered++;
+				dropSound.play();
+				iter.remove();
+			}
+		}
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void show() {
+		// start the playback of the background music
+		// when the screen is shown
+		rainMusic.play();
+	}
+
+	@Override
+	public void hide() {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public void dispose() {
+		dropImage.dispose();
+		bucketImage.dispose();
+		dropSound.dispose();
+		rainMusic.dispose();
+	}
+
+}
